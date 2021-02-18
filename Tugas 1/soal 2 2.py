cb163060a9218b2ab77cad08caaa86d0baf9933c
haekalghifari <haekalghfr@gmail.com>
2021-02-18 16:46:01 +0700

    basic python

---
+++ Tugas 1/soal 2 2.py
@@ -0,0 +1,12 @@
+print(" ")
+import math as m
+a = m.pi
+print(type(a))
+
+r=float(input("input any number for radius "))
+
+b = a*r**2
+
+print("luas lingkaran dengan jari jari {:.2f} cm adalah {:.2f} cm\u00b2".format(a,b))
+
+print(" ")
